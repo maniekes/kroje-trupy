849044a2b1e6ca3f337a30b4c8f8526d8070a91a
rafik <rafal@klimonda.pl>
2024-02-12 23:44:57 +0100

    better print layout

--- src/app/autopsy-display/autopsy-display.component.ts
+++ src/app/autopsy-display/autopsy-display.component.ts
@@ -1,13 +1,19 @@
 import {Component, Input} from '@angular/core';
 import {DatePipe} from "@angular/common";
 import {AutopsyProtocol} from "../core/models/autopsy-protocol.model";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-autopsy-display',
   standalone: true,
-    imports: [
-        DatePipe
-    ],
+  imports: [
+    DatePipe,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    MatCardSubtitle
+  ],
   templateUrl: './autopsy-display.component.html',
   styleUrl: './autopsy-display.component.scss'
 })
